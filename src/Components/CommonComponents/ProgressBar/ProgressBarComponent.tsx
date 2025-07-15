8ea5cebf6239002954a986fde76630d80cf0f5ce
Raksha <rakshac29@gmail.com>
2025-07-15 15:18:16 +0530

    fixed

--- src/Components/CommonComponents/ProgressBar/ProgressBarComponent.tsx
+++ src/Components/CommonComponents/ProgressBar/ProgressBarComponent.tsx
@@ -4,10 +4,10 @@ import { ProgressBar } from ".";
 export const ProgressBarComponent = () => {
   const [bars, setBars] = useState([]);
 
-  const handleClick = () => {
-    const barId = Date.now();
-    setBars((prevState) => [...prevState, barId]);
-  };
+  // const handleClick = () => {
+  //   const barId = Date.now();
+  //   setBars((prevState) => [...prevState, barId]);
+  // };
 
   useEffect(()=>{
    const timeId= setTimeout(()=>{
@@ -23,12 +23,12 @@ export const ProgressBarComponent = () => {
   
   return (
     <>
-      <button
-        onClick={handleClick}
+      {/* <button
+        //onClick={handleClick}
         className="px-6 py-2 bg-black text-white border-2 border-black"
       >
         Add Bars
-      </button>
+      </button> */}
       {bars?.map((id) => (
         <>
           <ProgressBar key={id} />
